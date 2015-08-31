0c803952f3697c959429ec909a8b9b418438bb5c
Kimihiro n <kimihiro_n@pistatium.dip.jp>
2015-08-31 21:57:05 +0900

    Init google app engine project

---
+++ appengine_config.py
@@ -0,0 +1,9 @@
+"""
+`appengine_config.py` is automatically loaded when Google App Engine
+starts a new instance of your application. This runs before any
+WSGI applications specified in app.yaml are loaded.
+"""
+
+from google.appengine.ext import vendor
+
+vendor.add('libraries')
