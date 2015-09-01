1b03aa7e99bf921915fea9193e74e668cb4b32da
Kimihiro N <kimihiro.nisimura@gmail.com>
2015-09-01 09:35:24 +0900

    Modify project structure

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ TomoriNao is a joke twitter client, changes all username to 友利奈緒(Tomori 
     * GoogleAppEngine SDK
 
 ```
+git submodule init
 mkvirtualenv --system-site-packages --distribute --python=python2.7 tomorinao
 pip install -r requirements.txt -t libraries
 dev_appserver.py .
--- appengine_config.py
+++ appengine_config.py
@@ -3,7 +3,12 @@
 starts a new instance of your application. This runs before any
 WSGI applications specified in app.yaml are loaded.
 """
-
+import os
+import sys
 from google.appengine.ext import vendor
+
+ROOTPATH = os.path.dirname(__file__)
+SRCPATH = os.path.join(ROOTPATH, 'src')
+sys.path.append(SRCPATH)
 
 vendor.add('libraries')
