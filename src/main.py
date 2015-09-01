9b4a3e3b0a12fd09270f6b7339fdff830b8a7b8e
Kimihiro n <kimihiro_n@pistatium.dip.jp>
2015-09-01 21:17:36 +0900

    Imprements oauth login

--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
 # coding: utf-8
 
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect
+
+from oauth import oauth
+
+from setting import *
+
 
 app = Flask(__name__)
+app.debug = True
+
+consumer_key = "LKlkj83kaio2fjiudjd9...etc"
+consumer_secret = "58kdujslkfojkjsjsdk...etc"
+callback_url = "http://www.myurl.com/callback/twitter"
+twitter_client = oauth.TwitterClient(CONSUMER_KEY, CONSUMER_SECRET, CALLBACK_URL)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/twitter_login')
+def twitter_login():
+    return redirect(twitter_client.get_authorization_url())
+
+@app.route('/twitter_callback')
+def twitter_callback():
+    auth_token = request.args.get("oauth_token", '')
+    auth_verifier = request.args.get("oauth_verifier", '')
+    user_info = twitter_client.get_user_info(auth_token, auth_verifier=auth_verifier)
+    import json
+    return json.dumps(user_info)
+
+
