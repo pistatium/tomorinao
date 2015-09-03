f0c6e9e9f262ea1162fb86ad165a91b25b25d02d
Kimihiro N <kimihiro.nisimura@gmail.com>
2015-09-03 09:23:27 +0900

    Fix user login

--- src/models/user.py
+++ src/models/user.py
@@ -11,16 +11,15 @@ class User(ndb.Model, UserMixin):
     secret = ndb.StringProperty()
 
     @classmethod
-    def get_by_id(cls, unique_id):
-        return cls.get_by_key_name(unique_id)
-
-    @classmethod
     def load(cls, user_info):
         return cls(
-            id = "{}:{}".format(user_info["service"], user_info["id"]),
-            screen_name = user_info["username"],
-            display_name = user_info["name"],
-            profile_image_url = user_info["picture"],
-            token = user_info["token"],
-            secret = user_info["secret"]
+            id="{}:{}".format(user_info["service"], user_info["id"]),
+            screen_name=user_info["username"],
+            display_name=user_info["name"],
+            profile_image_url=user_info["picture"],
+            token=user_info["token"],
+            secret=user_info["secret"]
         )
+
+    def get_id(self):
+        return self.key.id()
